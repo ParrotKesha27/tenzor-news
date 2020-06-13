941fe7b89de998a713b46d47a4b07921f9ad05df
Ignat Krasnov <parrotkesha27@gmail.com>
2020-06-13 15:32:14 +0300

    add openapi

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 
+SWAGGER_URL = '/api/docs'
+OPENAPI_PATH = '/static/openapi.yaml'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    OPENAPI_PATH,
+    config={
+        'app_name': "Tenzor-news"
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 @app.route('/')
 def hello_world():
