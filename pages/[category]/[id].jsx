976f19fd2fccb5e445a15c208a333462e905420d
vv.koshelev <kosha-lev97@yandex.ru>
2020-06-15 02:36:21 +0300

    add post page

--- pages/[category]/[id].jsx
+++ pages/[category]/[id].jsx
@@ -2,34 +2,38 @@ import { fetchPost } from '../../components/utils'
 import Layout from '../../components/layout'
 import styles from '../../styles/post.module.css'
 import Link from 'next/link'
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import Error from 'next/error'
 
 export default function Post({data}) {
-	/*const {title, author, category, createdAt, viewsCount, tags} = data;
+	if (data.message) {
+		return <Error statusCode={404} />
+	}
+	const {title, author, category, createdAt, viewsCount, tags} = data;
 	const content = decodeURI(data.content)
-	*/
-	const title = 'AJ:LGH:SDLJFL:SDjf'
-	const author = 'Vector'
-	const date = '12 июня 2020 17:45'
-	const viewsCount = '1240'
-	const tags = [{ name: 'tag1', slug: 'tag1' }]
-	const category = {name: 'Фильмы', slug: 'films'}
-	const content = encodeURI('<p>Hello!</p>')
+
 	const htmlContent = { __html: decodeURI(content) }
 	const tagsList = tags.map(({name, slug}) => 
-		<Link href={'/tag/'+slug}>
+		<Link key={name} href={'/tag/'+slug}>
 			<a className={styles.hashtag}>{name}</a>
 		</Link>
 	)
+
 	return (
 		<Layout>
 			<div className={styles.header}>
 				<div className={styles.info}>
 					<span>{author}</span>
-					<span>{category.name}</span>
-					<span>{date}</span>
-					<span>{viewsCount}</span>
+					<span>
+						<Link href={'/' + category.slug}>
+							<a className={styles.link}>{category.name}</a>
+						</Link>
+					</span>
+					<span>{createdAt}</span>
+					<span><VisibilityIcon style={{fontSize: 'large', marginRight: '8px'}}/>{viewsCount}</span>
 				</div>
 				<div className={styles.title}>{title}</div>
+				<hr />
 			</div>
 			<div className={styles.content} 
 				dangerouslySetInnerHTML={htmlContent}></div>
@@ -37,21 +41,15 @@ export default function Post({data}) {
 		</Layout>
 	)
 }
-/*
-export async function getStaticPaths() {
+
+export async function getServerSideProps(ctx) {
+	const { category, id } = ctx.params
+	
+	const res = await fetchPost(category, id)
+	const data = await res.json()
 	return {
-	    paths: [],
-	    fallback: true
-  }
+		props: {
+		 	data
+		}
+	}
 }
-
-export async function getStaticProps({id}) {
-  
-  const data = await fetchPost('memes', id)
-
-  return {
-    props: {
-      data
-    }
-  }
-}*/
